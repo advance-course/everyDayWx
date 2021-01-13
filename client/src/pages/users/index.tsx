c7a5e9f3f10054c4e5820c40ddc8dba9b566b660
yjp1998 <yejianpei1998@163.com>
2021-01-13 17:05:01 +0800

    修改页面样式

--- client/src/pages/users/index.tsx
+++ client/src/pages/users/index.tsx
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from 'react'
 import Taro from '@tarojs/taro'
-import { View, Button, Image, Text } from '@tarojs/components'
+import { View, Button, Image, Text, Input } from '@tarojs/components'
 import {userListApi} from 'api/user'
 import './index.scss'
+
+const userTypeDesc = {
+  1: '超级管理员',
+  2: '管理员',
+  3: '普通用户',
+  4: '尊贵VIP',
+}
 
 export default function UserPage() {
   const [data, setData] = useState({list: []})
   useEffect(async() => {
     const response = await userListApi();
     setData(response.data)
+    console.log(response.data)
   }, [])
   return(
     <View className='user_list'>
@@ -16,7 +24,13 @@ export default function UserPage() {
       data.list.map(item => (
         <View className='user_item' key={item.id}>
           <Image className='user_avatar' src={item.avatarUrl}></Image>
-          <Text className='user_nickName'>昵称：{`${item.nickName}`}</Text>
+          <View className="user_desc">
+            <Text className='user_nickName'>{`${item.nickName}`}</Text>
+            <Text className="user_city">{item.city ? `${item.city}` : '未填写'}</Text>
+          </View>
+          <View className="user_type">
+            <Text className="type_content">{`${userTypeDesc[item.type]}`}</Text>
+          </View>
         </View>
       ))
     }
